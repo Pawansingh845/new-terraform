bf8eb681d9ce7771ac0db89bf2d98e15b2bffa93
Pawansingh845 <pawan.ardom@gmail.com>
2026-05-27 05:50:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "name" {
     name = "mukesh"
     location = "eastus"
-#mukesh class chhod k bhag gaya
+#mukesh class chhod k bhag gaya , lgta hai call karna padega 
   
 }
